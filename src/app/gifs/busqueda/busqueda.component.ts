e74f0978e8d1a957cf06f13fffc4ecd8553b54e9
Gerson Angel <gerson.angel@ab-inbev.com>
2022-01-12 20:56:30 -0500

    ViewChild Obtener referencias a objetos del HTML

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -6,11 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent {
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  buscar() {
+    const valor = this.txtBuscar.nativeElement.value;
+    console.log('valor', valor);
+    this.txtBuscar.nativeElement.value = '';
   }
-
 }
